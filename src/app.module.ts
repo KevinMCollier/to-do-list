9c9047aeb48ef5d705551f5b5079e58b653e9ac2
kevin collier <collier.kevin.m@gmail.com>
2023-11-14 19:23:10 +0900

    updated forRoot connection for mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/to_do_list'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
