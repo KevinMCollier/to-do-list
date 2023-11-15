b6b3cad3dd4bc45fc554bfcaaf7b067d87da0dbc
kevin collier <collier.kevin.m@gmail.com>
2023-11-15 16:32:13 +0900

    updated error handling to Bad Request Exception

--- src/todo/services/todo.services.ts
+++ src/todo/services/todo.services.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateTodoDto } from '../dto/create-todo.dto';
@@ -61,7 +61,7 @@ export class TodoService {
     } else {
       // Handle the error if the day is not valid
       // This could be throwing an exception or handling it another way depending on your application's requirements
-      throw new Error('Invalid day of the week.');
+      throw new BadRequestException('Invalid day of the week.');
     }
   }
 }
