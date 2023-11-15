e4fff504da2014e9c4b8e9802c1930341e322e54
kevin collier <collier.kevin.m@gmail.com>
2023-11-15 16:19:51 +0900

    added daily-weekdays and daily-weekends to enum

--- src/todo/models/todo.model.ts
+++ src/todo/models/todo.model.ts
@@ -4,7 +4,7 @@ export const TodoSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: false },
   date: { type: Date, required: true },
-  repeat: { type: String, required: true, enum: ['Never', 'Daily', 'Weekly'] },
+  repeat: { type: String, required: true, enum: ['Never', 'Daily - Weekdays', 'Daily - Weekends', 'Daily', 'Weekly'] },
   dayOfWeek: { type: String, enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'], required: false },
   excludeWeekends: { type: Boolean, default: false, required: false },
 });
@@ -13,7 +13,7 @@ export interface Todo extends mongoose.Document {
   title: string;
   description?: string;
   date: Date;
-  repeat: 'Never' | 'Daily' | 'Weekly';
+  repeat: 'Never' | 'Daily - Weekdays' | 'Daily - Weekends' | 'Daily' | 'Weekly';
   dayOfWeek?: 'Monday' | 'Tuesday' | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday';
   excludeWeekends?: boolean;
 }
