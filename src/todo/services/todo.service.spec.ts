668684f6985c1bd42cba8b46ec4008168fa9a847
kevin collier <collier.kevin.m@gmail.com>
2023-11-18 10:13:27 +0900

    added unit test for checking never repeat status

--- src/todo/services/todo.service.spec.ts
+++ src/todo/services/todo.service.spec.ts
@@ -13,15 +13,16 @@ describe('TodoService', () => {
     _id: new mongoose.Types.ObjectId(),
     title: 'Test Todo',
     repeat: 'Never',
-    date: new Date(),
+    date: new Date('2023-01-01'),
     user: new mongoose.Types.ObjectId().toString(),
     save: jest.fn().mockResolvedValue(this),
   };
 
   const mockModel = {
     create: jest.fn().mockResolvedValue(mockTodo),
-    find: jest.fn(),
-    // Add other methods as needed
+    find: jest.fn().mockReturnValue({
+      exec: jest.fn(),
+    }),
   };
 
   beforeEach(async () => {
@@ -37,6 +38,11 @@ describe('TodoService', () => {
 
     service = module.get<TodoService>(TodoService);
     model = module.get(getModelToken('Todo'));
+
+    // Mock the behavior of exec for different test cases
+    mockModel.find().exec
+      .mockResolvedValueOnce([mockTodo]) // Mock return value for the first call
+      .mockResolvedValueOnce([]); // Mock return value for the second call
   });
 
   it('should be defined', () => {
@@ -58,5 +64,18 @@ describe('TodoService', () => {
     expect(model.create).toHaveBeenCalledWith(mockCreateTodoDto);
   });
 
-  // Add other tests here
+  it('should return Todo with "Never" repeat status if current date matches Todo date', async () => {
+    const currentDate = new Date('2023-01-01');
+
+    const result = await service.findTodosByDate(currentDate);
+    expect(result).toContainEqual(mockTodo);
+    expect(result.length).toBe(1);
+  });
+
+  it('should not return Todo with "Never" repeat status if current date does not match Todo date', async () => {
+    const currentDate = new Date('2023-01-02'); // different date
+
+    const result = await service.findTodosByDate(currentDate);
+    expect(result).toEqual([]);
+  });
 });
